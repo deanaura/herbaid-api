5f76f8a602117c503f79ca83301a393c6205b313
dean aura <deanaura58@gmail.com>
2023-12-19 18:16:34 +0700

    first commit

--- src/controllers/herbalController.js
+++ src/controllers/herbalController.js
@@ -1,6 +1,7 @@
+// Import library yang diperlukan dan konfigurasi Firebase
 const { v4: uuidv4 } = require("uuid");
 const { storage, uploadBytes, getDownloadURL, ref } = require("../config/firebase");
-const { db, collection, addDoc, getDoc, doc } = require("../config/firebase"); // Import 'doc' from firebase
+const { db, collection, addDoc, getDoc, doc } = require("../config/firebase");
 const HerbalModel = require("../models/herbalModel");
 
 // Fungsi untuk mengunggah gambar ke Firebase Storage
@@ -21,45 +22,33 @@ const uploadImageToStorage = async (file) => {
   }
 };
 
-// Gunakan fungsi ini untuk mengunggah file
-const fileInput = document.querySelector('input[type="file"]');
-fileInput.addEventListener("change", async (event) => {
-  const file = event.target.files[0];
-  if (file) {
-    await uploadImageToStorage(file);
-  }
-});
-
 // Fungsi untuk menyimpan data herbal yang teridentifikasi ke Firestore
 const saveIdentifiedHerbalData = async (identifiedHerbal) => {
   try {
     const herbalRef = collection(db, "herbals");
     const newHerbalDoc = await addDoc(herbalRef, { name: identifiedHerbal });
 
-    return { id: newHerbalDoc.id, name: identifiedHerbal }; // Return ID dan nama herbal yang disimpan
+    return { id: newHerbalDoc.id, name: identifiedHerbal };
   } catch (error) {
     throw error;
   }
 };
 
-
-// Fungsi untuk mengelola permintaan identifikasi herbal dari endpoint
-exports.identifyHerbal = async (req, res) => {
+// Fungsi untuk mendapatkan resep berdasarkan ID herbal
+exports.getRecipesByHerbal = async (req, res) => {
   try {
-    if (!req.file) {
-      return res.status(400).json({ error: "No image provided" });
+    const { herbalId } = req.params;
+    const recipesRef = db.collection('Recipes').where('herbalId', '==', herbalId);
+    const snapshot = await recipesRef.get();
+
+    if (snapshot.empty) {
+      res.status(404).json({ message: "Recipes not found for this herbal" });
+      return;
     }
 
-    // Proses gambar untuk identifikasi herbal
-    const imageUrl = await uploadImageToStorage(req.file);
+    const recipes = snapshot.docs.map(doc => doc.data());
 
-    // Placeholder untuk hasil identifikasi (nama herbal)
-    const identifiedHerbal = "Nama Herbal yang Teridentifikasi";
-
-    // Simpan hasil identifikasi ke Firestore
-    const savedHerbal = await saveIdentifiedHerbalData(identifiedHerbal);
-
-    res.status(200).json(savedHerbal);
+    res.status(200).json({ recipes });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -74,7 +63,6 @@ exports.getHerbalDetail = async (req, res) => {
 
     if (herbalSnapshot.exists()) {
       const herbalData = herbalSnapshot.data();
-      // Lakukan proses jika diperlukan untuk memformat atau memanipulasi data herbalData
       res.status(200).json({ herbalData });
     } else {
       res.status(404).json({ message: "Herbal not found" });
@@ -84,26 +72,31 @@ exports.getHerbalDetail = async (req, res) => {
   }
 };
 
-// Fungsi untuk mendapatkan resep berdasarkan ID herbal
-exports.getRecipesByHerbal = async (req, res) => {
+// Fungsi untuk identifikasi herbal dari gambar yang diunggah
+exports.identifyHerbal = async (req, res) => {
   try {
-    const { herbalId } = req.params;
-    const recipesRef = db.collection('Recipes').where('herbalId', 'array-contains', herbalId);
-    const snapshot = await recipesRef.get();
-
-    if (snapshot.empty) {
-      res.status(404).json({ message: "Recipes not found for this herbal" });
-      return;
+    if (!req.file) {
+      return res.status(400).json({ error: "No image provided" });
     }
 
-    const recipes = [];
-    snapshot.forEach((doc) => {
-      recipes.push({ id: doc.id, ...doc.data() }); // Mengambil ID dokumen dan data resep
-    });
+    // Proses identifikasi herbal dari gambar
+    const imageUrl = await uploadImageToStorage(req.file);
+    // Lakukan proses identifikasi herbal menggunakan imageUrl
+    const identifiedHerbal = "Nama Herbal yang Teridentifikasi"; // Ganti dengan hasil identifikasi sebenarnya
 
-    res.status(200).json({ recipes });
+    const savedHerbal = await saveIdentifiedHerbalData(identifiedHerbal);
+
+    res.status(200).json(savedHerbal);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
+// Gunakan fungsi ini untuk mengunggah file
+const fileInput = document.querySelector('input[type="file"]');
+fileInput.addEventListener("change", async (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    await uploadImageToStorage(file);
+  }
+});
