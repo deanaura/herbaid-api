7d61d565727f263241b3aed621ef157d9033ec9e
dean aura <deanaura58@gmail.com>
2023-12-19 16:58:31 +0700

    first commit

--- src/routes/herbalRoutes.js
+++ src/routes/herbalRoutes.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const multer = require('multer'); 
+const express = require("express");
+const multer = require("multer");
 
-const HerbalController = require('../controllers/herbalController');
-const { authMiddleware } = require('../middlewares/authMiddleware');
+const HerbalController = require("../controllers/herbalController");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
-const upload = multer(); 
+const upload = multer();
 
-router.post('/identify', authMiddleware, upload.single('herbalImage'), HerbalController.identifyHerbal);
+// Endpoint untuk identifikasi herbal
+router.post("/identify", authMiddleware, upload.single("herbalImage"), HerbalController.identifyHerbal);
+
+// Endpoint untuk mendapatkan detail herbal berdasarkan ID herbal
+router.get("/:herbalId", HerbalController.getHerbalDetail);
+
+// Endpoint untuk mendapatkan resep yang terkait dengan herbal berdasarkan ID herbal
+router.get("/:herbalId/recipes", HerbalController.getRecipesByHerbal);
 
 module.exports = router;
