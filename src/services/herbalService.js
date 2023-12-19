28cc0eaa95d4c4173fd4f41374e41a4972e0292a
dean aura <deanaura58@gmail.com>
2023-12-19 22:05:31 +0700

    first commit

--- src/services/herbalService.js
+++ src/services/herbalService.js
@@ -3,6 +3,10 @@ const { db, collection, query, where, getDocs } = require("../config/firebase");
 // Fungsi untuk mendapatkan resep berdasarkan ID herbal dari koleksi 'Recipes'
 const getRecipesByHerbalId = async (herbalId) => {
   try {
+    if (!herbalId) {
+      throw new Error("Invalid herbal ID provided");
+    }
+
     const recipesRef = collection(db, "recipes");
     const q = query(recipesRef, where("herbalId", "array-contains", herbalId));
     const querySnapshot = await getDocs(q);
