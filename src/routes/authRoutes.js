aa3b05bd9f609da5d8fb010f31c0862ae76b1f83
dean aura <deanaura58@gmail.com>
2023-12-22 00:20:08 +0700

    first commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { signup, signin, logout, getUserDetailsById } = require("../controllers/auth");
 const { authMiddleware } = require("../middlewares/authMiddleware");
-const { getUserById } = require("../models/userModel");
 
 const router = express.Router();
 
